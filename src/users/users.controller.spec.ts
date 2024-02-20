2005c89db1af7679042cdb9df021d7fc3b109913
barsukov-d <analitik.asdf.ge@gmail.com>
2024-02-20 09:41:11 +0300

    Refactor unit tests for UsersController and UsersService

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,18 +3,34 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+	let controller: UsersController;
+	let service: UsersService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-      providers: [UsersService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [UsersController],
+			providers: [
+				{
+					provide: UsersService,
+					useValue: {
+						create: jest.fn().mockResolvedValue('create'),
+						findAll: jest.fn().mockResolvedValue('findAll'),
+						findOne: jest.fn().mockResolvedValue('findOne'),
+						update: jest.fn().mockResolvedValue('update'),
+						remove: jest.fn().mockResolvedValue('remove'),
+					},
+				},
+			],
+		}).compile();
 
-    controller = module.get<UsersController>(UsersController);
-  });
+		controller = module.get<UsersController>(UsersController);
+		service = module.get<UsersService>(UsersService);
+	});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it('should call UsersService.create and return the result', async () => {
+		expect(await controller.create({} as any)).toBe('create');
+		expect(service.create).toBeCalledTimes(1);
+	});
+
+	// Добавьте здесь тесты для других методов
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { getModelToken } from '@nestjs/sequelize';
 
 describe('UsersService', () => {
-  let service: UsersService;
+	let service: UsersService;
+	let userModel: typeof User;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				UsersService,
+				{
+					provide: getModelToken(User),
+					useValue: {
+						create: jest.fn().mockResolvedValue({
+							appStatus: 1,
+							data: { email: 'test@test.com', id: 1 },
+							error: null,
+						}),
+						findAll: jest.fn().mockResolvedValue([
+							{
+								email: 'test@test.com',
+								id: 1,
+							},
+						]),
+						findOne: jest.fn().mockResolvedValue(null),
+						// добавьте моки для других методов, которые вы используете в UsersService
+					},
+				},
+			],
+		}).compile();
 
-    service = module.get<UsersService>(UsersService);
-  });
+		service = module.get<UsersService>(UsersService);
+		userModel = module.get<typeof User>(getModelToken(User));
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should call User.findAll and return the result', async () => {
+		const expectedResponse = {
+			appStatus: 1,
+			data: [{ email: 'test@test.com', id: 1 }],
+			error: null,
+		};
+
+		expect(await service.findAll()).toEqual(expectedResponse);
+		expect(userModel.findAll).toBeCalled();
+	});
+
+	// Добавьте здесь тесты для других методов
 });
